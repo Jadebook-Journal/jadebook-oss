11ecd41b2173a91a3446ce44f3ff20a6b4bbadd0
Tanvir Bhachu <46297124+tanvirbhachu@users.noreply.github.com>
2025-07-30 00:44:31 +0800

    allow users to set auth image and enable turborepo env

--- apps/platform/src/app/(auth)/layout.tsx
+++ apps/platform/src/app/(auth)/layout.tsx
@@ -3,7 +3,7 @@ import { AuthFormWrapper } from "@/components/auth/page";
 import { QueryProvider } from "@/providers/query-provider";
 
 const AUTH_IMAGE_SRC =
-	process.env.NEXT_PUBLIC_AUTH_IMAGE ||
+	process.env.NEXT_PUBLIC_AUTH_IMAGE_URL ||
 	"https://images.unsplash.com/photo-1570451488142-71d08e1511e3?w=1600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjF8fGFlc3RoZXRpY3xlbnwwfHwwfHx8Mg%3D%3D";
 
 export default function AuthLayout({
